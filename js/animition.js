066209adcf8e3acf9e015eedc5737247f5823075
wskkku <78215185+wskkku@users.noreply.github.com>
2021-04-02 18:05:03 +0800

    Update animition.js

--- js/animition.js
+++ js/animition.js
@@ -1,13 +1,20 @@
 //视察效果(速率，起始点，结束点)
 
-function parallax(rate, s, e) {
+function parallax(rate, s, e, t) {
     var ParallaxS = document.querySelector('.parallax-crolling')
+    ParallaxS.style.position = 'relative'
+
+    var start = s === undefined ? 0 : s
+    var end = e === undefined ? document.documentElement.scrollHeight : e
+    var top = t === undefined ? 0 : t
+
+    var boxTop = ParallaxS.offsetTop
 
     document.addEventListener('scroll', function () {
-        var start = s === undefined ? 0 : s
-        var end = e === undefined ? document.documentElement.scrollHeight : e
         if (window.pageYOffset >= start && window.pageYOffset <= end)
             var height = (window.pageYOffset - start) * rate
-        ParallaxS.style.transform = 'translateY(' + height + 'px)'
+        ParallaxS.style.top = top + height + 'px'
+        ParallaxS.style.transitionDuration = '0s'
+        console.log(ParallaxS.style.top);
     })
 }
